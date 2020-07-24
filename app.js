0af56d1035f56652f73cfce7d01ea40144370a1f
bruno <brunometal25@hotmail.com>
2020-07-24 10:54:25 -0300

    1.0

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,8 +19,47 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://admin-bruno:Test-123@cluster0-shard-00-00.asts3.mongodb.net:27017,cluster0-shard-00-01.asts3.mongodb.net:27017,cluster0-shard-00-02.asts3.mongodb.net:27017/blogPost?ssl=true&replicaSet=atlas-nq04oc-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.info('connection ok');
+    }
+  });
+
+const postSchema = {
+  title: String,
+  text: String,
+};
+
+const Post = mongoose.model("Post", postSchema)
+
+//List
+
+const listSchema = {
+  name: String,
+  items: [postSchema]
+};
+
+const List = mongoose.model("List", listSchema)
+
+
+
 app.get("/", function (req, res) {
-  res.render("home", { contentBody: homeStartingContent, posts: posts});
+
+  Post.find(function (err, foundPosts) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(foundPosts);
+      res.render("home", { contentBody: homeStartingContent, posts: foundPosts});
+    }
+  });
+
+
+ 
 })
 
 app.get("/about", function (req, res) {
@@ -35,12 +75,21 @@ app.get("/compose", function (req, res) {
 })
 
 app.post("/compose", function (req, res) {
-  const post = {
+
+  const post = new Post({
     title: req.body.postTitle,
     text: req.body.postBody,
-  }
-  posts.push(post);
+  });
+
+  post.save();
   res.redirect("/");
+
+  // const post = {
+  //   title: req.body.postTitle,
+  //   text: req.body.postBody,
+  // }
+  // posts.push(post);
+  // res.redirect("/");
 })
 
 app.get("/posts/:postName", function (req, res) {
@@ -55,17 +104,12 @@ app.get("/posts/:postName", function (req, res) {
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
+
